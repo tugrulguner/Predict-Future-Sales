ef12f86a195fcc111d705ea835c1d9990cc40eff
Tugrul Guner <47668043+tugrulguner@users.noreply.github.com>
2020-04-29 23:55:18 -0400

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -9,8 +9,6 @@ from sklearn.preprocessing import StandardScaler
 
 train = pd.read_csv('/kaggle/input/competitive-data-science-predict-future-sales/sales_train.csv')
 
-#train['item_price'] = train['item_price'][train['item_price'].between(train['item_price'].quantile(.1), train['item_price'].quantile(.9))]
-
 sns.regplot(y="item_price", x="shop_id", data=train, x_estimator = np.mean)
 
 sns.regplot(y="item_price", x="date_block_num", data=train, x_estimator = np.mean)
@@ -24,6 +22,9 @@ def namecorr(column):
     return name[0:2], name[5:]
     
 aa= train['date'].apply(namecorr)
+
+# Here we are using namecorr we defined above the split 'Date' values in 'date' column into month and year values in order to
+# be able to add 'month' and 'year' columns to dataset.
 
 b = pd.DataFrame(aa.values.tolist(), columns = ['month','year'])
 train = pd.concat([train, b], axis = 1)
@@ -45,10 +46,10 @@ deger = 0
 degerson = 0 
 
 while degerson < 0.85:
-    X_train, X_test, y_train, y_test = train_test_split(train.drop(['item_price', 'date', 'date_block_num', 'item_cnt_day'], axis = 1), 
+    X_train, X_test, y_train, y_test = train_test_split(train.drop(['item_price', 'date_block_num', 'item_cnt_day'], axis = 1), 
                                                          train['item_cnt_day'], test_size=0.3)
     
-    modelf = RandomForestClassifier(n_estimators = 50) # These values were determined through Randomized Search CV above 
+    modelf = RandomForestRegressor(n_estimators = 50)
     modelf.fit(X_train, y_train) 
     deger = cross_validate(modelf, X_test, y_test, cv=4) # I like to use cross validation to determine my score
     degerson = deger['test_score'].mean()
@@ -69,16 +70,13 @@ while degermson < 0.85:
     X_train, X_test, y_train, y_test = train_test_split(train.drop(['item_price', 'date', 'date_block_num'], axis = 1), 
                                                          train['date_block_num'], test_size=0.3)
     
-    models = RandomForestClassifier(n_estimators = 50) # These values were determined through Randomized Search CV above 
+    models = RandomForestClassifier(n_estimators = 50) 
     models.fit(X_train, y_train) 
-    degerm = cross_validate(models, X_test, y_test, cv=4) # I like to use cross validation to determine my score
+    degerm = cross_validate(models, X_test, y_test, cv=4) 
     degermson = degerm['test_score'].mean()
     m += 1
     if m>1:
         break
-    # Here I put certain contraint on the loop, if I couldn't find score higher than 0.85, It will stop after some certain 
-    # iteration. I can improve this part here to involve even though it couldnt find better score than 0.85, it can select
-    # the best one it got during the process.
 
 degerm['test_score'].mean()
 
@@ -90,16 +88,14 @@ while degerbson < 0.85:
     X_train, X_test, y_train, y_test = train_test_split(train.drop(['item_price', 'date'], axis = 1), 
                                                          train['item_price'], test_size=0.3)
     
-    modelt = RandomForestRegressor(n_estimators = 50) # These values were determined through Randomized Search CV above 
+    modelt = RandomForestRegressor(n_estimators = 50) 
     modelt.fit(X_train, y_train) 
-    degerb = cross_validate(modelt, X_test, y_test, cv=4) # I like to use cross validation to determine my score
+    degerb = cross_validate(modelt, X_test, y_test, cv=4) 
     degerbson = degerb['test_score'].mean()
     m += 1
     if m>1:
         break
-    # Here I put certain contraint on the loop, if I couldn't find score higher than 0.85, It will stop after some certain 
-    # iteration. I can improve this part here to involve even though it couldnt find better score than 0.85, it can select
-    # the best one it got during the process.
+
 
 degerb['test_score'].mean()
 
